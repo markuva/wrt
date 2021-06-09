8af214ec424e537771d3aa255c576b99ca75358f
markuva <83833049+markuva@users.noreply.github.com>
2021-06-09 17:39:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,10 +12,10 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-rm -rf feeds/packages/libs/libcap  ##啥
+#rm -rf feeds/packages/libs/libcap  ##啥
 rm -rf package/feeds/routing/olsrd/patches/012-gpsd.patch ##啥
 rm -rf feeds/packages/net/smartdns      ##广告大师？？
 
-svn co https://github.com/openwrt/openwrt/trunk/package/libs/libcap
+#svn co https://github.com/openwrt/openwrt/trunk/package/libs/libcap
 sed -i 's/DEPENDS.*/& \+luci-i18n-mwan3-zh-cn/g'  package/lean/luci-app-syncdial/Makefile
 sed -i 's/DEPENDS.*/& \+kmod-usb-core +kmod-usb-ohci +kmod-usb-uhci +kmod-usb-storage +kmod-usb-storage-extras +kmod-usb2 +kmod-scsi-core/g'  package/lean/automount/Makefile
