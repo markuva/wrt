b7e1af2db0d1fe3ca1aecc21f53e8087cb36df39
markuva <83833049+markuva@users.noreply.github.com>
2021-06-10 12:44:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,10 +9,3 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-rm -rf feeds/packages/libs/libcap
-
-svn co https://github.com/openwrt/openwrt/trunk/package/libs/libcap                                      feeds/packages/libs/libcap
-
-sed -i 's/DEPENDS.*/& \+luci-i18n-mwan3-zh-cn/g'  package/lean/luci-app-syncdial/Makefile
-sed -i 's/DEPENDS.*/& \+kmod-usb-core +kmod-usb-ohci +kmod-usb-uhci +kmod-usb-storage +kmod-usb-storage-extras +kmod-usb2 +kmod-scsi-core/g'  package/lean/automount/Makefile
-
